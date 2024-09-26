1c0f343d5360fcac1eb99a67b7cfdd469890c648
anishkumar157 <anishkumarnaik5@gmail.com>
2024-09-26 21:58:43 +0530

    variable and datatypes

---
+++ VariableName.java
@@ -0,0 +1,34 @@
+public class VariableName{
+	public static void main(String[] args){
+
+		byte numberOfWindowsInHouse = 8;
+		byte asciiValueOfAlphabet = 65;
+		byte batterypercentage = 83;
+		byte monthInYear = 12;
+		byte maxAttempts = 5;
+		byte maxSpeedLimit = 120;
+		byte maxScore = 100;
+		byte floorNumber = 10;
+
+		System.out.println("ASCII value of A: "+asciiValueOfAlphabet);
+		asciiValueOfAlphabet = 97;
+		System.out.println("ASCII value of a: "+asciiValueOfAlphabet);
+
+
+		short hieghtInInches = 65;
+		short temperatureInFahrenheit = 72;
+		short numberOfPagesInBook = 192;
+		short speedOfVehicle = 190;
+		short numberOfRoomsInFlat = 200;
+		short vehiclesInParkingSlots = 350;
+		short totalMarks = 1200;
+		short daysInYear = 365;
+
+		System.out.println("Speed of a fast moving vehicle:"+speedOfVehicle);
+		speedOfVehicle = 180;
+		System.out.println("Speed of a fast moving vehicle:"+speedOfVehicle);
+
+
+
+	}
+}
