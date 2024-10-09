a724fee5c68ef70798382dea737e20f329975adf
anishkumar157 <anishkumarnaik5@gmail.com>
2024-10-10 01:10:58 +0530

    Method Overloading And Variable Command Line

--- MethodOverloadingExample.java
+++ MethodOverloadingExample.java
@@ -1,26 +1,39 @@
 public class MethodOverloadingExample{
 
 	public static void main(String[] args) {
+		
 
 		System.out.println("*****************Area calculator method****************");
-		// deffferent datatype with same method_name
+		
 		areaCalculator(16);
 		areaCalculator(10.5f);
-		// same name and defferent parameater number
 		areaCalculator(8 ,7.5f);
-		// same name and defferent order of parameter
 		areaCalculator(7.5f, 6);
-		System.out.println("*****************Area calculator method****************");
-		// deffferent datatype with same method_name
+
+
+		System.out.println("*****************Personal Details method****************");
+
 		personDetails("Anish");
 		personDetails(24);
-		// same name and defferent parameater number
 		personDetails("Anish" ,24);
-		// same name and defferent order of parameter
 		personDetails(24, "Anish");
 
-	}
 
+		System.out.println("*****************Details of Stocks in Warehouse method****************");
+
+		stocksInWarehouse("Phones");
+		stocksInWarehouse(2400);
+		stocksInWarehouse("Phones", 2400, 125000.40f);
+		stocksInWarehouse(125000.45f, "phones", 2400);
+
+
+		System.out.println("****************Example for Variable argument  method****************");
+
+		findAverage(8, 6, 2, 56);
+
+	}
+// fist method
+	// deffferent datatype with same method_name
 	public static void areaCalculator(int sides){
 		int areaOfSquare = sides * sides;
 		System.out.println("Area of Square = "+ areaOfSquare);
@@ -32,10 +45,14 @@ public class MethodOverloadingExample{
 
 	}
 
+		// same name and defferent parameater number
+
 	public static void areaCalculator(int length, float width){
 		float areaOfSquare = length * width;
 		System.out.println("Area of Rectangle = "+ areaOfSquare);
 	}
+
+	// same name and defferent order of parameter
 
 	public static void areaCalculator(float width, int length){
 		float areaOfSquare = width * length;
@@ -43,6 +60,8 @@ public class MethodOverloadingExample{
 
 	}
 
+// second method
+	// deffferent datatype with same method_name
 	public static void personDetails(String name){
 		System.out.println(name);
 	}
@@ -51,12 +70,54 @@ public class MethodOverloadingExample{
 		System.out.println(age);
 	}
 
+			// same name and defferent parameater number
+
+
 	public static void personDetails(String name, int age){
 		System.out.println(name+" "+age);
 	}
 
+	// same name and defferent order of parameter
+
 	public static void personDetails(int age, String name){
 		System.out.println(name+" "+age);
 	}
+
+
+// third method
+	// deffferent datatype with same method_name
+	public static void stocksInWarehouse(String itemName){
+		System.out.println(itemName);
+	}
+
+	public static void stocksInWarehouse(int numberOfStocks){
+		System.out.println(numberOfStocks);
+	}
+
+			// same name and defferent parameater number
+
+
+	public static void stocksInWarehouse(String itemName, int numberOfStocks, float price){
+		System.out.println("name: "+itemName+" Price: "+price+" Number of Stock Left: "+numberOfStocks);
+	}
+
+	// same name and defferent order of parameter
+
+	public static void stocksInWarehouse(float price, String itemName, int numberOfStocks){
+	System.out.println("name: "+itemName+" Price: "+price+" Number of Stock Left: "+numberOfStocks);
+	}
+
+// 4 th method for variable argument example.
+
+	public static void findAverage(int... value){
+		int sum=0;
+		int avg=0;
+		for(int i=0; i<value.length;i++){
+			sum+=value[i];
+		}
+		avg=sum/value.length;
+		System.out.println("average= "+avg);
+	}
+
 
 }
