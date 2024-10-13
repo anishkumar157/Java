05db0f0affd08a0ffffabf8e58d41603025f2488
anishkumar157 <anishkumarnaik5@gmail.com>
2024-10-13 16:26:04 +0530

    Class & Object Session

---
+++ HotelRooms.java
@@ -0,0 +1,146 @@
+public class HotelRooms{
+
+	public static void main(String[] args) {
+
+		RoomInfo room1 = new RoomInfo();
+		room1.roomNumber = 101;
+		room1.type = "Double";
+		room1.price = 800;
+		room1.availability = "Yes";
+		room1.maxNumberOfpeople = 3;
+
+		RoomInfo room2 = new RoomInfo();
+		room2.roomNumber = 102;
+		room2.type = "Double";
+		room2.price = 800;
+		room2.availability ="Yes" ;
+		room2.maxNumberOfpeople = 4;
+
+		RoomInfo room3 = new RoomInfo();
+		room3.roomNumber = 103;
+		room3.type = "Double";
+		room3.price = 900;
+		room3.availability ="Yes" ;
+		room3.maxNumberOfpeople = 5;
+
+		RoomInfo room4 = new RoomInfo();
+		room4.roomNumber = 104;
+		room4.type = "Single";
+		room4.price = 400;
+		room4.availability ="False" ;
+		room4.maxNumberOfpeople = 2;
+
+		RoomInfo room5 = new RoomInfo();
+		room5.roomNumber = 105;
+		room5.type = "Double";
+		room5.price = 1200;
+		room5.availability = "Yes";
+		room5.maxNumberOfpeople = 4;
+
+		RoomInfo room6 = new RoomInfo();
+		room6.roomNumber = 106;
+		room6.type = "Double";
+		room6.price = 800;
+		room6.availability = "Yes";
+		room6.maxNumberOfpeople = 3;
+
+		RoomInfo room7 = new RoomInfo();
+		room7.roomNumber = 107;
+		room7.type = "Single";
+		room7.price = 500;
+		room7.availability = "Yes";
+		room7.maxNumberOfpeople = 1;
+
+		RoomInfo room8 = new RoomInfo();
+		room8.roomNumber = 108;
+		room8.type = "Double";
+		room8.price = 1000;
+		room8.availability = "False";
+		room8.maxNumberOfpeople = 2;
+
+		RoomInfo room9 = new RoomInfo();
+		room9.roomNumber = 109;
+		room9.type = "Double";
+		room9.price = 1100;
+		room9.availability = "False";
+		room9.maxNumberOfpeople = 4;
+
+		RoomInfo room10 = new RoomInfo();
+		room10.roomNumber = 110;
+		room10.type = "Double";
+		room10.price = 700;
+		room10.availability = "Yes";
+		room10.maxNumberOfpeople = 3;
+
+		System.out.println("********************************************");
+		System.out.println("Room Number: "+room1.roomNumber);
+		System.out.println("Room Type: "+room1.type);
+		System.out.println("Room Price: "+room1.price);
+		System.out.println("Room Availability: "+room1.roomNumber);
+		System.out.println("Max no.of People allowed: "+room1.roomNumber);
+
+		System.out.println("********************************************");
+		System.out.println("Room Number: "+room2.roomNumber);
+		System.out.println("Room Type: "+room2.type);
+		System.out.println("Room Price: "+room2.price);
+		System.out.println("Room Availability: "+room2.roomNumber);
+		System.out.println("Max no.of People allowed: "+room2.roomNumber);
+
+		System.out.println("********************************************");
+		System.out.println("Room Number: "+room3.roomNumber);
+		System.out.println("Room Type: "+room3.type);
+		System.out.println("Room Price: "+room3.price);
+		System.out.println("Room Availability: "+room3.roomNumber);
+		System.out.println("Max no.of People allowed: "+room3.roomNumber);
+
+		System.out.println("********************************************");
+		System.out.println("Room Number: "+room4.roomNumber);
+		System.out.println("Room Type: "+room4.type);
+		System.out.println("Room Price: "+room4.price);
+		System.out.println("Room Availability: "+room4.roomNumber);
+		System.out.println("Max no.of People allowed: "+room4.roomNumber);
+
+		System.out.println("********************************************");
+		System.out.println("Room Number: "+room5.roomNumber);
+		System.out.println("Room Type: "+room5.type);
+		System.out.println("Room Price: "+room5.price);
+		System.out.println("Room Availability: "+room5.roomNumber);
+		System.out.println("Max no.of People allowed: "+room5.roomNumber);
+
+		System.out.println("********************************************");
+		System.out.println("Room Number: "+room6.roomNumber);
+		System.out.println("Room Type: "+room6.type);
+		System.out.println("Room Price: "+room6.price);
+		System.out.println("Room Availability: "+room6.roomNumber);
+		System.out.println("Max no.of People allowed: "+room6.roomNumber);
+
+		System.out.println("********************************************");
+		System.out.println("Room Number: "+room7.roomNumber);
+		System.out.println("Room Type: "+room7.type);
+		System.out.println("Room Price: "+room7.price);
+		System.out.println("Room Availability: "+room7.roomNumber);
+		System.out.println("Max no.of People allowed: "+room7.roomNumber);
+
+		System.out.println("********************************************");
+		System.out.println("Room Number: "+room8.roomNumber);
+		System.out.println("Room Type: "+room8.type);
+		System.out.println("Room Price: "+room8.price);
+		System.out.println("Room Availability: "+room8.roomNumber);
+		System.out.println("Max no.of People allowed: "+room8.roomNumber);
+
+		System.out.println("********************************************");
+		System.out.println("Room Number: "+room9.roomNumber);
+		System.out.println("Room Type: "+room9.type);
+		System.out.println("Room Price: "+room9.price);
+		System.out.println("Room Availability: "+room9.roomNumber);
+		System.out.println("Max no.of People allowed: "+room9.roomNumber);
+
+		System.out.println("********************************************");
+		System.out.println("Room Number: "+room10.roomNumber);
+		System.out.println("Room Type: "+room10.type);
+		System.out.println("Room Price: "+room10.price);
+		System.out.println("Room Availability: "+room10.roomNumber);
+		System.out.println("Max no.of People allowed: "+room10.roomNumber);
+
+	}
+}
