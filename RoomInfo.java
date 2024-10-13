dc8ac9c088a10c4723f1202f0d6c01fd76f90730
anishkumar157 <anishkumarnaik5@gmail.com>
2024-10-13 16:25:28 +0530

    Class & Object Session

---
+++ RoomInfo.java
@@ -0,0 +1,24 @@
+public class RoomInfo{
+
+	int roomNumber;
+	String type;   
+    double price;
+    String availability;
+    int maxNumberOfpeople; 
+
+    public static void bookRooom(){
+
+    } 
+
+    public static void checkavailability(){
+    	
+    } 
+
+    public static void displayRoomDetails(){
+    	
+    } 
+
+    public static void applyDiscount(){
+    	
+    } 
+}
